2c4724e6963f860fd55384cbc7f38c4e1b19ac3d
CatGirlKyori <168188981+CatGirlKyori@users.noreply.github.com>
2026-03-16 13:24:55 +0000

    more issues

--- Scripts/ItemObject.gd
+++ Scripts/ItemObject.gd
@@ -1,11 +1,13 @@
-extends GameManager
+extends Node
 class_name ItemObject
 
 func _ready() -> void:
 	return
 
 func on_body_entered(body) -> void:
+	print ("collide")
 	if body.is_in_group("player"):
 		print ("collide")
-		get_node("Level/Node3D2").visible =false
+		queue_free()
 		print ("collide")
+ 
