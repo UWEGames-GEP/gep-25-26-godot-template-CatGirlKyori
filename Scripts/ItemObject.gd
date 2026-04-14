dc54470f063a3bb2980e3d05608e1f67df2bee9d
CatGirlKyori <Harrison2.Roberts@live.uwe.ac.uk>
2026-04-14 12:13:09 +0100

    I have no clue why this isn't working

--- Scripts/ItemObject.gd
+++ Scripts/ItemObject.gd
@@ -1,13 +1,15 @@
-extends Node
+extends Area3D
 class_name ItemObject
 
 func _ready() -> void:
+	print("test")
 	return
 
 func on_body_entered(body) -> void:
 	print ("collide")
-	if body.is_in_group("player"):
-		print ("collide")
-		queue_free()
-		print ("collide")
+	queue_free()
+	#if body.is_in_group("Player"):
+	#	print ("collide")
+	#	queue_free()
+	#	print ("collide")
  
