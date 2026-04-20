f5fbd1992faa060afc5ee314242094bfba7c9f79
CatGirlKyori <Harrison2.Roberts@live.uwe.ac.uk>
2026-04-20 07:24:42 +0100

    idk how to fix

--- Scripts/ItemObject.gd
+++ Scripts/ItemObject.gd
@@ -4,12 +4,13 @@ class_name ItemObject
 func _ready() -> void:
 	print("test")
 	return
+	
 
 func on_body_entered(body) -> void:
 	print ("collide")
 	queue_free()
-	#if body.is_in_group("Player"):
-	#	print ("collide")
-	#	queue_free()
-	#	print ("collide")
+	if body.is_in_group("Player"):
+		print ("collide")
+		queue_free()
+		print ("collide")
  
